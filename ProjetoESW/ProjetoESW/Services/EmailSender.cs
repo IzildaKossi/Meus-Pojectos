22ba30bc6c3ddd511f314cd82e7d93e207731fc3
Danilson Sanches <dsanches06@outlook.com>
2019-03-16 15:32:41 +0000

    add project

--- ProjetoESW/ProjetoESW/Services/EmailSender.cs
+++ ProjetoESW/ProjetoESW/Services/EmailSender.cs
@@ -1,9 +1,5 @@
 ﻿using Microsoft.Extensions.Options;
-using SendGrid;
-using SendGrid.Helpers.Mail;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Net.Mail;
 using System.Threading.Tasks;
 
 namespace ESW19Backup2.Services
@@ -26,17 +22,19 @@ namespace ESW19Backup2.Services
 
         public Task Execute (string apiKey, string subject, string message, string email)
         {
-            var client = new SendGridClient(apiKey);
-            var msg = new SendGridMessage()
-            {
-                From = new EmailAddress("AlberCool@pt.pt", "AlberCool"),
-                Subject = subject,
-                PlainTextContent = message,
-                HtmlContent = message
-            };
+            /* var client = new SendGridClient(apiKey);
+             var msg = new SendGridMessage()
+             {
+                 From = new MailAddress("AlberCool@pt.pt", "AlberCool"),
+                 Subject = subject,
+                 PlainTextContent = message,
+                 HtmlContent = message
+             };
 
-            msg.AddTo(new EmailAddress(email));
-            return client.SendEmailAsync(msg);
+             msg.AddTo(new MailAddress(email));
+             return client.SendEmailAsync(msg);
+             */
+            return null;
         }
     }
 }
